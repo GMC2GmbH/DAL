7cfdc3271b0263eee89348b2381710844f5e80f7
Alexander Gusser <alexander.gusser@gmail.com>
2023-09-05 17:20:50 +0200

    initial repo for dal

---
+++ DAL.py
@@ -0,0 +1,160 @@
+from ast import Try
+from typing_extensions import Self
+import yaml
+import json
+from TM1py.Services import TM1Service
+from TM1py.Utils import Utils
+from collections import namedtuple
+from typing import overload
+import os
+import logging
+
+
+""" Class DataAccessLayer manages all relevant pull and push requests """
+
+
+class DataAccessLayer:
+
+    __version__ = 'V0.13'
+    __author__ = 'Alexander Gusser'
+
+    def __init__(self, path):
+        with open(os.path.join(os.path.dirname(__file__), path)) as f:
+            self.config = yaml.load(f, Loader=yaml.FullLoader)
+            self.tm1 = _PlanningAnalytics(config=self.config)
+
+    def show_config(self):
+        print(self.config)
+
+    def load_data_by_mdx(self, mdx, raw=False):
+        """load cube data by mdx statement
+
+        Args:
+            mdx (string): the mdx statement should contain all the needed parts -> SELECT [...] on ROWS, [...] on COLUMNS, FROM [...] WHERE([..]).
+            raw (bool, optional): Determine return value neither as raw cellset data or as pandas.DataFrame.
+
+        Returns:
+            pandas.DataFrame: Two-dimensional, size-mutable, potentially heterogeneous tabular data.
+        """
+
+        return self.tm1.data_by_mdx(mdx, raw)
+
+    def load_data_by_view(self, cube, view, raw=False):
+        """load cube data by view
+
+        Args:
+            cube (string): name of the corresponding cube
+            view (string): name of the cube view
+
+        Returns:
+            pandas.DataFrame: Two-dimensional, size-mutable, potentially heterogeneous tabular data.
+        """
+
+        return self.tm1.data_by_view(cube, view, raw)
+
+    def write_cellset(self, cube, cellset):
+        self.tm1.write_cellset(cube, cellset)
+        
+    def write_dataframe(self, cube, dataframe):
+        self.tm1.write_dataframe(cube, dataframe)
+        
+    def run_ti(self, name, parameters=None):
+        self.tm1.run_ti(name,parameters)
+        
+    #JOD  
+    def show_all_cubes(self):
+        print(self.tm1.cubes.get_all())
+
+
+""" _PlanningAnalytics is an internal class with the purpose of establishing communication between Python and Planning Analytics """
+
+class _PlanningAnalytics:
+
+    __version__ = 'V0.1'
+    __author__ = 'Alexander Gusser'
+
+    def __init__(self, config):
+        # get and set config!
+        self.config = namedtuple("Config", config["planning_analytics"].keys())(
+            *config["planning_analytics"].values())
+
+        # Check the connection!
+        self.check_connection()
+
+    def _get_service(self):
+        """returns the TM1Service
+
+        Returns:
+            TM1py.Services: to establish the connection!
+        """
+        return TM1Service(address=self.config.address, port=self.config.port, user=self.config.user, password=self.config.password, ssl=self.config.ssl)
+
+    def check_connection(self):
+        try:
+            with self._get_service() as tm1:
+                server = tm1.server.get_configuration()
+                # log
+                logging.info(f'connection {server["ServerName"]} [V{server["ProductVersion"]}] is established!')
+        except Exception as e:
+            logging.error(f'error: {str(e)}')
+
+    def data_by_mdx(self, mdx, raw=False):
+        """load planning analytics cube cells by mdx statement
+
+        Args:
+            mdx (String): mdx statement with all necessary information
+        """
+        with self._get_service() as tm1:
+            # Get data from cube through MDX
+            data = tm1.cubes.cells.execute_mdx(mdx)
+
+            if(raw):
+                # return raw cellset data
+                return data
+            else:
+                # Build pandas DataFrame from raw cellset data
+                return Utils.build_pandas_dataframe_from_cellset(data)
+
+    def data_by_view(self, cube, view, raw=False):
+        with self._get_service() as tm1:
+            # Get data from cube through View
+            data = tm1.cubes.cells.execute_view(cube, view, private=False)
+
+            if(raw):
+                # return raw cellset data
+                return data
+            else:
+                # Build pandas DataFrame from raw cellset data
+                return Utils.build_pandas_dataframe_from_cellset(data)
+
+    def write_dataframe(self, cube, dataframe):
+        try:
+            with self._get_service() as tm1: 
+                # write new values
+                tm1.cubes.cells.write_dataframe(cube_name=cube, data=dataframe, use_ti=True)
+                # log
+                logging.info(f'dataframe in {cube} are stored!')
+        except Exception as e:
+            logging.error(f'error: {str(e)}')
+            
+    def write_cellset(self, cube, cellset):
+        try:
+            with self._get_service() as tm1:
+                # write new values
+                tm1.cubes.cells.write_values(cube_name=cube, cellset_as_dict=cellset)
+                # log
+                logging.info(f'cellset in {cube} are stored!')
+        except Exception as e:
+            logging.error(f'error: {str(e)}')
+    
+    def run_ti(self, name,parameters=None):
+        try:
+            with self._get_service() as tm1:
+                #get process
+                process =  tm1.processes.get(name_process=name)
+                # write new values
+                sucess, status, error_log_file = tm1.processes.execute_process_with_return(process,**parameters)
+                # log
+                logging.info(f'process {name} executed! {sucess} | {status}')
+        except Exception as e:
+            logging.error(f'error: {str(e)}')
--- README.md
+++ README.md
@@ -1,1 +1,113 @@
-# DAL
+# DAL 
+
+Data-driven innovation through the use of the data access layer
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) 
+![ibm](https://img.shields.io/badge/-PlanningAnalytics-052FAD?logo=ibm&style=for-the-badge) 
+
+Install
+=======================
+> copy the following files to your python development environment
+- DEV.yml
+- DAL.py
+
+Config
+=======================
+```yaml
+planning_analytics:
+    address: <url to your ibm planning analytics instance>
+    port: <http-port>
+    user: <username>
+    password: <password>
+    ssl: <boolean>
+```
+
+Use
+=======================
+> import
+
+``` python
+from DAL import DataAccessLayer
+```
+
+> instantiate the Data Access Layer with the DEV environment
+``` python
+DAL = DataAccessLayer(path='DEV.yml')
+```
+
+> build example mdx-statement
+``` python
+mdx = {
+    'columns':'{TM1FILTERBYLEVEL(TM1SUBSETALL([col.dim].[col.dim]) , 0)}',
+    'rows':'{TM1FILTERBYLEVEL(TM1SUBSETALL([row.dim].[row.dim]) , 0)}',
+    'where':[
+        '[where.dim].[where.dim].[member]'
+    ]
+}
+
+query = f"SELECT NON EMPTY {mdx['columns']} ON 0, NON EMPTY {mdx['rows']} ON 1 FROM [etl.data] WHERE ({','.join(mdx['where'])})"
+```
+
+> Call the Data Access Layer PULL request by MDX-Statement
+``` python
+data_raw = DAL.load_data_by_mdx(query)
+```
+
+Helper
+=======================
+
+> convert pandas multi-index to flat df
+``` python
+data = data_raw.dropna()
+data = data.unstack()
+data = data.reset_index()
+data.columns = data.columns.map(''.join)
+```
+
+> Run TI
+``` python
+DAL.run_ti(name="test",parameters={'pnValue':1234})
+```
+
+Features
+=======================
+
+DAL has more tricks up its sleeve than a magician's hat when it comes to getting cozy with TM1py, such as
+
+- Read data from TM1 via MDX and views as easy as possible 
+- Write/Push data from outside into TM1 
+- Retrieve TI processes from outside 
+- Easily configurable via YAML files 
+- Significantly more interfaces of source/target systems can be connected (compared to TurboIntegrator) - e.g. OData 
+- Core technology extending application 
+- Secure and encrypted connection via SSL; data is not transferred in plain text 
+- Data transfer from cloud to cloud using the authorization scheme specified by the source and target system in each case
+
+
+Requirements
+=======================
+
+- python (3.7 or higher)
+- requests
+- TM1 11 
+- TM1py
+
+Optional Requirements
+=======================
+
+- pandas
+- requests_negotiate_sspi
+
+
+Issues
+=======================
+
+If you find issues, sign up in Github and open an Issue in this repository
+
+
+Contribution
+=======================
+
+DAL is like a magical show, where everyone in the TM1 community can become a wizard! 🧙‍♂️
+
+If you happen to spot a pesky bug or have a spellbinding idea to enhance it, simply take your magical wand (fork the repository), weave your code incantations, and then conjure up a pull request. We'll be ready to merge in your enchanting contributions! ✨🪄
